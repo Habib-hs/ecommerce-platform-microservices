8117c4a57a42779cfd89204625bdf7841da2c7d3
Habib-hs <mhabibullahhs@gmail.com>
2025-04-21 12:02:27 +0600

    added controller file

--- product-service/src/main/java/com/habib/microservices/product/controller/ProductController.java
+++ product-service/src/main/java/com/habib/microservices/product/controller/ProductController.java
@@ -1,9 +1,29 @@
 package com.habib.microservices.product.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.habib.microservices.product.dto.ProductRequest;
+import com.habib.microservices.product.dto.ProductResponse;
+import com.habib.microservices.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
+        return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
 }
