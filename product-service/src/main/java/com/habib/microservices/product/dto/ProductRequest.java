06e4ca781e59465ee949a75d40ee92d657d4a4de
Habib-hs <mhabibullahhs@gmail.com>
2025-04-21 10:56:43 +0600

    package dto added

---
+++ product-service/src/main/java/com/habib/microservices/product/dto/ProductRequest.java
@@ -0,0 +1,9 @@
+package com.habib.microservices.product.dto;
+
+import java.math.BigDecimal;
+
+public record ProductRequest(String id, String name, String description,
+                              BigDecimal price) {
+
+
+}
